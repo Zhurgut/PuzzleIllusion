1e664c776329e5569a89eaa6499123680ead05bd
zhurgut <damian.ca@bluewin.ch>
2026-04-06 12:15:40 +0200

    refactor using the helpers

--- generate_image.py
+++ generate_image.py
@@ -6,6 +6,37 @@ pipeline = helpers.pipeline
 
 
 def optimize(
+    latents, prompt_embeds, num_inference_steps, guidance_scale, begin_index=0, nr_steps=None
+):
+
+    helpers.prepare_scheduler(num_inference_steps, begin_index)
+
+    nr_steps_to_take = num_inference_steps - begin_index if nr_steps is None else min(nr_steps, num_inference_steps - begin_index)
+    assert begin_index + nr_steps_to_take <= num_inference_steps
+
+    # 7. Denoising loop
+    with pipeline.progress_bar(total=nr_steps_to_take) as progress_bar:
+        for i in range(begin_index, begin_index + nr_steps_to_take):
+
+            t = pipeline.scheduler.timesteps[i:i+1]
+
+            noise_pred = helpers.get_noise_pred2(latents, prompt_embeds, t, guidance_scale)
+
+            # compute the previous noisy sample x_t -> x_t-1
+            # x_t = (1-t)*x_0 + t*epsilon
+            latents = pipeline.scheduler.step(noise_pred, t, latents, return_dict=False)[0]
+
+            # call the callback, if provided
+            if i == len(pipeline.scheduler.timesteps) - 1 or ((i + 1) > 0 and (i + 1) % pipeline.scheduler.order == 0):
+                progress_bar.update()
+
+    if begin_index + nr_steps_to_take == num_inference_steps:
+        return latents, None
+    else:
+        return latents, begin_index + nr_steps_to_take
+    
+
+def generate(
     prompt: str,
     height: int = pipeline.default_sample_size * pipeline.vae_scale_factor,
     width: int = pipeline.default_sample_size * pipeline.vae_scale_factor,
@@ -14,12 +45,7 @@ def optimize(
 ):
     with torch.no_grad():
 
-        prompt_embeds, negative_prompt_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds = pipeline.encode_prompt(
-            prompt=prompt,
-            prompt_2=prompt,
-            prompt_3=prompt,
-        )
-
+        prompt_embeds = helpers.encode_prompts(prompt)
 
         # 4. Prepare latent variables
         num_channels_latents = pipeline.transformer.config.in_channels
@@ -28,64 +54,24 @@ def optimize(
             num_channels_latents,
             height,
             width,
-            prompt_embeds.dtype,
+            prompt_embeds[0].dtype,
             "cuda",
             None,
             None,
         )
 
-        pipeline.scheduler.set_timesteps(num_inference_steps)
-        pipeline.scheduler.set_begin_index(0)
+        finished_latents, next_t = optimize(latents, prompt_embeds, num_inference_steps, guidance_scale)
 
-    
-        # 7. Denoising loop
-        with pipeline.progress_bar(total=num_inference_steps) as progress_bar:
-            for i in range(len(pipeline.scheduler.timesteps)):
-
-                t = pipeline.scheduler.timesteps[i:i+1]
-
-                noise_pred = pipeline.transformer(
-                    hidden_states=latents,
-                    timestep=t.to("cuda"),
-                    encoder_hidden_states=prompt_embeds,
-                    pooled_projections=pooled_prompt_embeds,
-                    return_dict=False,
-                )[0]
-
-                # perform guidance
-                if guidance_scale > 1:
-                    noise_pred_uncond = pipeline.transformer(
-                        hidden_states=latents,
-                        timestep=t.to("cuda"),
-                        encoder_hidden_states=negative_prompt_embeds,
-                        pooled_projections=negative_pooled_prompt_embeds,
-                        return_dict=False,
-                    )[0]
-
-                    noise_pred = noise_pred_uncond + guidance_scale * (noise_pred - noise_pred_uncond)
-
-
-                # compute the previous noisy sample x_t -> x_t-1
-                # x_t = (1-t)*x_0 + t*epsilon
-                latents = pipeline.scheduler.step(noise_pred, t, latents, return_dict=False)[0]
-
-
-                # call the callback, if provided
-                if i == len(pipeline.scheduler.timesteps) - 1 or ((i + 1) > 0 and (i + 1) % pipeline.scheduler.order == 0):
-                    progress_bar.update()
-
-        latents = (latents / pipeline.vae.config.scaling_factor) + pipeline.vae.config.shift_factor
-
-        dec = pipeline.vae.decode(latents, return_dict=False)[0]
-        image = pipeline.image_processor.postprocess(dec, output_type="pil")[0]
+        image = helpers.latent_to_pil(finished_latents)
 
         return image
 
 
-for i in range(10):
-    img = optimize(
-        "forest in the style of studio ghibli, anime style",
-        512, 512
-    )
+# for i in range(10):
+# img = generate(
+#     "forest in the style of studio ghibli, anime style",
+#     512, 512,
+#     num_inference_steps=10,
+# )
 
-    img.save("out/image.png")
+# img.save("out/image.png")
