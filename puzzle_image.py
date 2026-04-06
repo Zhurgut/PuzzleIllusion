c9174b65ffbf0161ece3959a6bd0654ae166d112
zhurgut <damian.ca@bluewin.ch>
2026-04-06 14:56:54 +0200

    batch computations of the two images

--- puzzle_image.py
+++ puzzle_image.py
@@ -43,41 +43,13 @@ def permute_latent(latents, permutex, permutey):
 
 
 
-def _generate(
-    prompt1: str,
-    prompt2: str,
+def optimize(
+    latents, 
+    prompt_embeds,
     num_inference_steps,
     guidance_scale,
-    nr_steps_to_look_ahead,
-    
+    permutex, permutey, invpermutex, invpermutey
 ):
-
-    prompt_embeds1 = helpers.encode_prompts(prompt1)
-    prompt_embeds2 = helpers.encode_prompts(prompt2)
-
-    datax = np.loadtxt("out/perm_x.csv", delimiter=',')
-    datay = np.loadtxt("out/perm_y.csv", delimiter=',')
-    permutex = (torch.from_numpy(datax) - datax.min()).long()
-    permutey = (torch.from_numpy(datay) - datay.min()).long()
-
-    height, width = permutex.shape
-
-    # 4. Prepare latent variables
-    num_channels_latents = pipeline.transformer.config.in_channels
-    latents1 = pipeline.prepare_latents(
-        1,
-        num_channels_latents,
-        height,
-        width,
-        prompt_embeds1[0].dtype,
-        "cuda",
-        None,
-        None,
-    )
-
-    latents2 = permute_latent(latents1, permutex, permutey)
-
-    invpermutex, invpermutey = inverse_permutation(permutex, permutey)
 
     pipeline.scheduler.set_timesteps(num_inference_steps)
     pipeline.scheduler.set_begin_index(0)
@@ -89,65 +61,34 @@ def _generate(
             t = pipeline.scheduler.timesteps[i:i+1]
             s = pipeline.scheduler.sigmas[i]
 
-            noise_pred1 = helpers.get_noise_pred2(latents1, prompt_embeds1, t, guidance_scale)
-            noise_pred2 = helpers.get_noise_pred2(latents2, prompt_embeds2, t, guidance_scale)
+            noise_preds = helpers.get_noise_pred(latents, prompt_embeds, t, guidance_scale)
 
             # hint_for_img2 = latents1 - s * noise_pred1
             # hint_for_img1 = latents2 - s * noise_pred2
 
-            future_latents1, next_t_idx = generate_image.optimize(latents1, prompt_embeds1, num_inference_steps, guidance_scale, begin_index=i, nr_steps=nr_steps_to_look_ahead)
-            future_latents2, next_t_idx = generate_image.optimize(latents2, prompt_embeds1, num_inference_steps, guidance_scale, begin_index=i, nr_steps=nr_steps_to_look_ahead)
+            future_latents = generate_image.optimize(latents, prompt_embeds, num_inference_steps, guidance_scale, begin_index=i)
 
-            if next_t_idx is not None:
-                timestep = pipeline.scheduler.timesteps[next_t_idx:next_t_idx+1]
-                sigma = pipeline.scheduler.sigmas[next_t_idx]
-
-                hint_noise_pred1 = helpers.get_noise_pred2(future_latents1, prompt_embeds1, timestep, guidance_scale)
-                hint_noise_pred2 = helpers.get_noise_pred2(future_latents2, prompt_embeds2, timestep, guidance_scale)
-                
-                future_latents1 = future_latents1 - sigma * hint_noise_pred1
-                future_latents2 = future_latents2 - sigma * hint_noise_pred2
-
-
-            hint_for_img2 = helpers.latents_roundtrip(future_latents1, permutex, permutey)
-            hint_for_img1 = helpers.latents_roundtrip(future_latents2, invpermutex, invpermutey)
+            future_img1, future_img2 = helpers.decode(future_latents).chunk(2)
+            hint_for_img2 = future_img1[:, :, permutey, permutex]
+            hint_for_img1 = future_img2[:, :, invpermutey, invpermutex]
+            hint_latents = helpers.encode2(torch.cat([hint_for_img1, hint_for_img2]))
             
         
-            hint_noise_pred1 = (latents1 - hint_for_img1) / (s + 0.01)
-            hint_noise_pred2 = (latents2 - hint_for_img2) / (s + 0.01)
+            hint_noise_preds = (latents - hint_latents) / (s + 0.01)
 
-            
-            # w = 0 if (1-s) > hint_until else 0.5 * hint_weight * math.sqrt(map(1-s, 0, hint_until, 4, 0))
-            w = 0.5
-            final_noise_pred1 = (1-w) * noise_pred1 + w * hint_noise_pred1
-            final_noise_pred2 = (1-w) * noise_pred2 + w * hint_noise_pred2
-            # dif1 = hint_noise_pred1 - noise_pred1
-            # dif2 = hint_noise_pred2 - noise_pred2
-            # lf1 = torch.nn.functional.avg_pool2d(dif1, 3, stride=1, padding=1)
-            # lf2 = torch.nn.functional.avg_pool2d(dif2, 3, stride=1, padding=1)
-            # lf1 = torchvision.transforms.functional.gaussian_blur(dif1, (5,5), sigma=0.7)
-            # lf2 = torchvision.transforms.functional.gaussian_blur(dif2, (5,5), sigma=0.7)
-            # TODO other idea, temporal averaging, might also wash out any high frequency stuff
-            # hint_noise_pred1 = 0.5 * hint_noise_pred1 + 0.5 * prev_hint_pred
-            # prev_hint_pred = hint_noise_pred1
-
-            # final_noise_pred1 = noise_pred1 + w * lf1
-            # final_noise_pred2 = noise_pred2 + w * lf2
+            final_noise_preds = 0.5 * (noise_preds + hint_noise_preds)
 
             helpers.prepare_scheduler(num_inference_steps, i)
 
-            latents1 = pipeline.scheduler.step(final_noise_pred1, t, latents1, return_dict=False)[0]
-            pipeline.scheduler._step_index -= 1
-            latents2 = pipeline.scheduler.step(final_noise_pred2, t, latents2, return_dict=False)[0]
+            latents = pipeline.scheduler.step(final_noise_preds, t, latents, return_dict=False)[0]
 
-            # call the callback, if provided
+            print()
             if i == len(pipeline.scheduler.timesteps) - 1 or ((i + 1) > 0 and (i + 1) % pipeline.scheduler.order == 0):
                 progress_bar.update()
 
     imgs = []
 
-    dec1 = helpers.decode(latents1)
-    dec2 = helpers.decode(latents2)
+    dec1, dec2 = helpers.decode(future_latents).chunk(2)
 
     dec1_permuted = dec1[:, :, permutey, permutex]
     dec2_invpermuted = dec2[:, :, invpermutey, invpermutex]
@@ -164,10 +105,38 @@ def generate(
     prompt2: str,
     num_inference_steps = 50,
     guidance_scale = 7.0,
-    nr_steps_to_look_ahead = 10,
 ):
     with torch.no_grad():
-        return _generate(prompt1, prompt2, num_inference_steps, guidance_scale, nr_steps_to_look_ahead)
+
+        prompt_embeds = helpers.encode_prompts([prompt1, prompt2])
+
+        datax = np.loadtxt("out/perm_x.csv", delimiter=',')
+        datay = np.loadtxt("out/perm_y.csv", delimiter=',')
+        permutex = (torch.from_numpy(datax) - datax.min()).long()
+        permutey = (torch.from_numpy(datay) - datay.min()).long()
+
+        height, width = permutex.shape
+
+        # 4. Prepare latent variables
+        num_channels_latents = pipeline.transformer.config.in_channels
+        latents = pipeline.prepare_latents(
+            2,
+            num_channels_latents,
+            height,
+            width,
+            prompt_embeds[0].dtype,
+            "cuda",
+            None,
+            None,
+        )
+
+        invpermutex, invpermutey = inverse_permutation(permutex, permutey)
+        
+        return optimize(
+            latents, prompt_embeds,
+            num_inference_steps, guidance_scale,
+            permutex, permutey, invpermutex, invpermutey
+        )
 
 
 imgs = generate(
@@ -180,10 +149,11 @@ imgs = generate(
     # "watercolor painting of a bunny",
     # "abstract oil painting of the face of an old man with a beard",
     # "abstract oil painting of a woman's face, with prominent eyes and red lips",
-    "a painting of houseplants in the style of studio ghibli, anime style",
-    "abstract illustration of marilyn monroe",
-    num_inference_steps=30,
-    nr_steps_to_look_ahead=20,
+    # "a painting of houseplants in the style of studio ghibli, anime style",
+    # "abstract illustration of marilyn monroe",
+    "a detailed color pencil drawing of exotic houseplants",
+    "abstract color pencil drawing of marilyn monroe",
+    num_inference_steps=50,
 )
 
 # imgs = generate(
