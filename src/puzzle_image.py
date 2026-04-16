b5fe9dea86e806195b94b35d8b4f90fa09e9e411
zhurgut <damian.ca@bluewin.ch>
2026-04-16 21:37:54 +0200

    optimization: only use fixed nr of steps to finish latents at each step

--- src/puzzle_image.py
+++ src/puzzle_image.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import helpers
 import image
+import os
 
 pipeline = helpers.pipeline
 
@@ -28,41 +29,31 @@ def inverse_permutation(permutex, permutey):
     return inv_permutex, inv_permutey
 
 
-
-def permute_latent(latents, permutex, permutey):
-    bs, c, h, w = latents.shape
-    pixels = torch.repeat_interleave(latents, 8, dim=-1)
-    pixels = torch.repeat_interleave(pixels, 8, dim=-2)
-    permuted = pixels[:, :, permutey, permutex]
-    permuted_latents = torch.nn.functional.avg_pool2d(permuted, 8, stride=8)
-    return permuted_latents
-
-
-
 def optimize(
     latents, 
     prompt_embeds,
     num_inference_steps,
+    nr_steps_to_finish,
     guidance_scale,
     permutex, permutey, invpermutex, invpermutey
 ):
-
-    pipeline.scheduler.set_timesteps(num_inference_steps)
-    pipeline.scheduler.set_begin_index(0)
 
     def map(x, a, b, A, B):
         return (x-a) / (b-a) * (B-A) + A
 
     # 7. Denoising loop
     with pipeline.progress_bar(total=num_inference_steps) as progress_bar:
-        for i in range(len(pipeline.scheduler.timesteps)):
+        for i in range(num_inference_steps):
+
+            helpers.prepare_scheduler(num_inference_steps, i)
 
             t = pipeline.scheduler.timesteps[i:i+1]
             s = pipeline.scheduler.sigmas[i]
 
         
             print()
-            future_latents = image.optimize(latents, prompt_embeds, num_inference_steps, guidance_scale, begin_index=i)
+            helpers.prepare_linear_schedule(min(nr_steps_to_finish, num_inference_steps-i), start=s)
+            future_latents = image.optimize(latents, prompt_embeds, guidance_scale)
 
             
             future_img1, future_img2 = helpers.decode(future_latents).chunk(2)
@@ -70,13 +61,6 @@ def optimize(
             hint_for_img2 = future_img1[:, :, permutey, permutex]
             hint_for_img1 = future_img2[:, :, invpermutey, invpermutex]
             hint_latents = helpers.encode2(torch.cat([hint_for_img1, hint_for_img2]))
-
-            if i % 5 == 0:
-                pipeline.image_processor.postprocess(future_img1, output_type="pil")[0].save(f"prog1/img{i}.png")
-                pipeline.image_processor.postprocess(future_img2, output_type="pil")[0].save(f"prog2/img{i}.png")
-
-                pipeline.image_processor.postprocess(hint_for_img1, output_type="pil")[0].save(f"prog1/omg{i}.png")
-                pipeline.image_processor.postprocess(hint_for_img2, output_type="pil")[0].save(f"prog2/omg{i}.png")
             
             # w = 0.1
             # latents = latents + w * (hint_latents - future_latents)
@@ -85,19 +69,15 @@ def optimize(
             hint_noise_preds = (latents - hint_latents) / s # s != 0 here always
 
             w_snr = s**2 / (s**2 + (1-s)**2)
-            w = map(w_snr, 1, 0, 0.7, 0.5)
+            w = map(w_snr, 1, 0, 0.75, 0.5)
             # w = 0.7
             final_noise_preds = (1-w) * noise_preds + w * hint_noise_preds
             # final_noise_preds = hint_noise_preds
-
-            
 
             helpers.prepare_scheduler(num_inference_steps, i)
 
             latents = pipeline.scheduler.step(final_noise_preds, t, latents, return_dict=False)[0]
 
-
-            
             if i == len(pipeline.scheduler.timesteps) - 1 or ((i + 1) > 0 and (i + 1) % pipeline.scheduler.order == 0):
                 progress_bar.update()
 
@@ -111,7 +91,6 @@ def optimize(
     imgs.append(pipeline.image_processor.postprocess(0.5 * (dec1 + dec2_invpermuted), output_type="pil")[0])
     imgs.append(pipeline.image_processor.postprocess(0.5 * (dec2 + dec1_permuted), output_type="pil")[0])
     
-
     return imgs
     
 
@@ -120,6 +99,7 @@ def generate(
     prompt1: str,
     prompt2: str,
     num_inference_steps = 32,
+    nr_steps_to_finish=14,
     guidance_scale = 7.0,
     negative_prompt1="",
     negative_prompt2="",
@@ -128,8 +108,10 @@ def generate(
 
         prompt_embeds = helpers.encode_prompts([prompt1, prompt2], [negative_prompt1, negative_prompt2])
 
-        datax = np.loadtxt(f"puzzles/{puzzle_w}x{puzzle_h}/perm_x.csv", delimiter=',')
-        datay = np.loadtxt(f"puzzles/{puzzle_w}x{puzzle_h}/perm_y.csv", delimiter=',')
+        dir_path = helpers.get_src_path()
+
+        datax = np.loadtxt(os.path.join(dir_path, "..", f"puzzles/{puzzle_w}x{puzzle_h}/perm_x.csv"), delimiter=',')
+        datay = np.loadtxt(os.path.join(dir_path, "..", f"puzzles/{puzzle_w}x{puzzle_h}/perm_y.csv"), delimiter=',')
         permutex = (torch.from_numpy(datax) - datax.min()).long()
         permutey = (torch.from_numpy(datay) - datay.min()).long()
 
@@ -152,7 +134,7 @@ def generate(
         
         return optimize(
             latents, prompt_embeds,
-            num_inference_steps, guidance_scale,
+            num_inference_steps, nr_steps_to_finish, guidance_scale,
             permutex.to("cuda"), 
             permutey.to("cuda"), 
             invpermutex.to("cuda"), 
@@ -160,10 +142,10 @@ def generate(
         )
 
 
-s = 0
-for i in range(10):
-    imgs = generate(
-        6,6, 
+# s = 0
+# for i in range(10):
+#     imgs = generate(
+#         8,8,
         # "painting of a fantasy castle with lots of trees",
         # "painting of a large fish close up, in an underwater landscape, with plants and corals.",
         # "oil painting of a large fish, close up, swimming in an underwater landscape, with plants, algae and corals",
@@ -177,27 +159,30 @@ for i in range(10):
         # "abstract color pencil drawing of marilyn monroe",
         # "watercolor of a duck in a lake",
         # "watercolor of a bunny with some grass and flowers around it",
+        # "watercolor of a duck, there are waterlillies and reeds",
+        # "watercolor of a bunny in the grass",
+        # "ink drawing of a robot",
+        # "ink drawing of a birthday cake",
         # "painting of a duck in the style of Picasso's cubism",
         # "painting of a bunny in the style of Picasso's cubism",
-        # "oil painting of a kitchen",
+        # "oil painting of a bowl of fruit on a kitchen table",# table with fruit bowl and decorations", 
         # "oil painting of a deer",
-        "close-up photo of a duck in a forest, there is moss and brown pine needles on the floor",
-        "close-up photo of a bunny in a pine forest, there are pine cones, moss and mushrooms",
+        # "painting of a bottle of wine",
+        # "minimalist art of a pole dancer",
+        # "alpine landscape",
+        # "a large intricate beautiful colorful flower",
+        # "nice sports car in the streets of tokyo, anime style, studio ghibli",
+        # "illustration of the countryside by night during full moon, in a field next to a small house, anime style",
+        # "close-up photo of a duck in a forest, there is moss and brown pine needles on the floor",
+        # "close-up photo of a bunny in a pine forest, there are pine cones, moss and mushrooms",
         # "a quick abstract charcoal sketch of a duck, with rough, approximate strokes",
         # "a quick abstract charcoal sketch of a bunny, with rough, approximate strokes",
         # negative_prompt1="realism, photography",
         # negative_prompt2="realism, photography",
-        num_inference_steps=40,
-        
-    )
-
-    # imgs = generate(
-    #     "a water color drawing of a duck",
-    #     "a water color drawing of a bunny",
-    #     
     #     num_inference_steps=50,
+        
     # )
 
-    for j in range(len(imgs)):
-        imgs[j].save(f"out/puzzle{s}.png")
-        s += 1
+    # for j in range(len(imgs)):
+    #     imgs[j].save(f"out/puzzle{s}.png")
+    #     s += 1
